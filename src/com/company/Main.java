549b23189a5ac7466c2d000bef7e6bec1fffbdf0
Radu <udarnicus@gmail.com>
2020-05-26 13:35:00 +0200

    Comment added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,11 @@
 package com.company;
 
 public class Main {
-
+    /**
+     * aaa
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 	// write your code here
     }
